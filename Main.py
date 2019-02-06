4eeb90c179e19b9e982845190d3a36506598c994
reidtc82 <reidtc82@gmail.com>
2019-02-06 13:24:18 -0800

    lunch work

--- Main.py
+++ Main.py
@@ -9,3 +9,23 @@ import Line from Line
 # 1. Create two line arrays and populate with new lines.
 # 2. Create array of boxes passing a boarder of lines to each - lines will be reused.
 # 3. Create starting state passing it the array of boxes as the "field."
+
+class Main:
+    def __init__(self, xdim, ydim, plies):
+        self.xdim = xdim
+        self.ydim = ydim
+        self.plies = plies
+        self.hlines = [] *self.ydim+1
+        self.vlines = [] *self.xdim+1
+
+        #build horizontal lines
+        for i in range(xdim):
+            for j in range(ydim+1):
+
+        #build vertical lines
+        for i in range(ydim):
+            for j in range(xdim+1):
+
+
+    def solve(self):
+        #Do all the solving here
